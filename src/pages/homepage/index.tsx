417f3ec289ccc1e9de6240bbf72d6850bf69d5d4
marcinfabisiak97 <marcinfab1@o2.pl>
2022-01-07 01:58:44 +0100

    adding hover animationt to devtechpart, makinf exppart

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -5,19 +5,21 @@ import Phonepart from "../../component/phonepart";
 import Controlpart from "../../component/controlpart";
 import Projectspart from "../../component/myprojectspart";
 import Devtech from "../../component/devtechgnologypart";
+import Exp from "../../component/experiencepart";
 const Homepage = () => {
-	const myPortfolio = useRef<HTMLDivElement>(null);
-	const myService = useRef<HTMLDivElement>(null);
-	const myPhone = useRef<HTMLDivElement>(null);
-	return (
-		<div>
-			<Menupart {...{ myPortfolio, myService, myPhone }} />
-			<Ideaspart {...{ myPortfolio, myService, myPhone }} />
-			<Phonepart {...{ myPortfolio, myService, myPhone }} />
-			<Controlpart />
-			<Projectspart />
-			<Devtech />
-		</div>
-	);
+  const myPortfolio = useRef<HTMLDivElement>(null);
+  const myService = useRef<HTMLDivElement>(null);
+  const myPhone = useRef<HTMLDivElement>(null);
+  return (
+    <div>
+      <Menupart {...{ myPortfolio, myService, myPhone }} />
+      <Ideaspart {...{ myPortfolio, myService, myPhone }} />
+      <Phonepart {...{ myPortfolio, myService, myPhone }} />
+      <Controlpart />
+      <Projectspart />
+      <Devtech />
+      <Exp />
+    </div>
+  );
 };
 export default Homepage;

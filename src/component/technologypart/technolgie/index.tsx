cc100399b8487122171ee15b54c141a5fac923d9
marcinfabisiak97 <marcinfab1@o2.pl>
2022-01-11 01:39:17 +0100

    correcting fiels names

--- src/component/technologypart/technolgie/index.tsx
+++ src/component/technologypart/technolgie/index.tsx
@@ -8,30 +8,30 @@ const Technolgie = (props) => {
   return (
     <React.Fragment>
       {expander ? (
-        <div className="technolgiesBricks technolgiesBricks--expand">
+        <div className="technologiesBricks technologiesBricks--expand">
           <img src={props.photo} alt="ui" />
-          <div className="technolgiesBricks__midleText">
+          <div className="technologiesBricks__midleText">
             <p>{props.number}</p>
             <h3>{props.kind}</h3>
             <p>{props.text} </p>
           </div>
           <img
-            className="technolgiesBricks__cliker technolgiesBricks__cliker--rotate"
+            className="technologiesBricks__cliker technologiesBricks__cliker--rotate"
             onClick={() => setExpander("")}
             src={rightPointer}
             alt="pointer"
           />
         </div>
       ) : (
-        <div className="technolgiesBricks">
+        <div className="technologiesBricks">
           <img src={props.photo} alt="ui" />
-          <div className="technolgiesBricks__midleText">
+          <div className="technologiesBricks__midleText">
             <p>{props.number}</p>
             <h3>{props.kind}</h3>
             <p>{props.text} </p>
           </div>
           <img
-            className="technolgiesBricks__cliker"
+            className="technologiesBricks__cliker"
             onClick={() => setExpander(props.number)}
             src={rightPointer}
             alt="pointer"
